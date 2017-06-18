f474bf9bb35dabdd563d5a8c16a192f160440d39
Kiwane Garner <kiwanegarner012@yahoo.com>
2017-06-17 21:18:55 -0700

    Changes to the modal

--- trello.js
+++ trello.js
@@ -1,14 +1,42 @@
-alert("You are connected");
-
 var addButton = document.querySelector("#addButton");
-var body = document.querySelector("body");
 var divFormBox = document.querySelector("#formBoxDisplay");
-//var formBoxDisplay = document.createElement("div");
+var label1 = document.querySelector("label");
+//var label2 = document.querySelector(".label2");
+//var label3 = document.querySelector(".label3");
+var userInput = document.querySelector("#textBox");
 
 addButton.addEventListener("click", function(){
 	console.log("add Button clicked");
 	divFormBox.classList.toggle("formBox");
-	
-	//formBoxDisplay.style.display = "none";
+
+	divFormBox.classList.toggle("label1");
+	if (label1.style.display === "block"){
+		label1.style.display = "none";
+	} else {
+		label1.style.display = "block";
+	}
+
+	divFormBox.classList.toggle("userInput");
+	if (userInput.style.display === "block"){
+		userInput.style.display = "none";
+	} else {
+		userInput.style.display = "block";
+	}
+
+	/*divFormBox.classList.toggle("label2");
+	label2.textContent = "Enter your profile name here:";
+	if (label2.style.display === "block"){
+		label2.style.display = "none";
+	} else {
+		label2.style.display = "block";
+	}
+
+	divFormBox.classList.toggle("label3");
+	label3.textContent = "This is placeholding text:";
+	if (label3.style.display === "block"){
+		label3.style.display = "none";
+	} else {
+		label3.style.display = "block";
+	}*/
 });
 
